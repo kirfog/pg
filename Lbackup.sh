40e2e366234ccf1646dfb623447088263f03d75f
Kir Fog <kirfog@gmail.com>
2020-10-22 12:02:17 +0300

    linux backup

--- Lbackup.sh
+++ Lbackup.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
-psql -U postgres -c "\l" | head -n-7 | tail -n +4 | while read -r str ; do
-		IFS=' '
-		read -ra ADDR <<< "$str"
-    	echo "${ADDR[0]}"
-    	pg_dump -U postgres -F c -b -f /root/${ADDR[0]}$(date +%Y%m%d) ${ADDR[0]}
+apath="/mnt/files/backup/"
+alog=$apath$(date +%Y%m%d)"log.txt"
+
+
+echo "***  $(date +%Y%m%d)  ***" >> $alog
+psql -U postgres -c "select * from pg_database" | head -n -2 | tail -n +3 | sed '/^ template/d' | sed '/^ postgres/d' | while read -r str ; do
+        IFS=' '
+        read -ra ADDR <<< "$str"
+        echo -e "\n${ADDR[0]}" >> $alog
+        /usr/bin/time -p -f %E -o $alog -a pg_dump -U postgres -F c -b -f $apath$(date +%Y%m%d)${ADDR[0]}.bak ${ADDR[0]} 2>>$apath$(date +%Y%m%d)err.txt
+        du -sh $apath$(date +%Y%m%d)${ADDR[0]}.bak >> $alog
 done
+echo "*** *** ***" >> $alog
+
+if  [ ! -s $apath$(date +%Y%m%d)err.txt ]
+then
+	echo -e "\n*** DELETED FILES ***" >> $alog
+	find $apath -type f -mtime +5 -delete >> $alog
+else
+	echo -e "\n*** ERROR ***" >> $alog
+	cat $apath$(date +%Y%m%d)err.txt >> $alog
+fi
+echo "*** *** ***" >> $alog
+
+echo -e "\n*** BACKUP FOLDER ***" >> $alog
+du -sh /mnt/files/backup/ >> $alog
+echo "*** *** ***" >> $alog
+
+echo -e "\n*** BACKUP DISK ***" >> $alog
+df -ha /mnt/files/backup/ >> $alog
+echo "*** *** ***" >> $alog
+
+echo -e "\n*** VACUUM ***" >> $alog
+/usr/bin/time -p -f %E -o $alog -a vacuumdb -U postgres -a -z -F -j 4 >> $alog
+echo "*** *** ***" >> $alog
+
+cat $alog | mail -s "PostgreSQL backup $(date +%Y%m%d)" -r 'PostgreSQL Backup <backup@domain.com>' admin@domain.com
--- Lrestore.sh
+++ Lrestore.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-while [ ! -f "/root/$base$day.bak" ]
-do
-	read -p "Database " -r base
-	read -p "Day as 20201231 " -r day
-	if [ ! -f "/root/$base$day.bak" ] ; then echo "Check check check"; fi
-done
-
+apath="/mnt/files/backup/"
+if [ $# -eq 0 ]
+then
+    echo "No archive database file supplied"
+    while [ ! -f "$apath$day$base.bak" ]
+    do
+        read -p "Archive database name " -r base
+        read -p "Archive database name date as 20201231 " -r day
+        if [ ! -f "$apath$day$base.bak" ] ; then echo "Check backup file"; fi
+    done
+fi
 while [ "$exist" != "n" ] && [ "$over" != "y" ]
 do
     read -p "New database " -r newbase
@@ -20,15 +24,29 @@ do
         else
                 exist=n
         fi
-     done < <(psql -U postgres -c "\l" | head -n-7 | tail -n +4)
-
+    done < <(psql -U postgres -c "select * from pg_database" | head -n -2 | tail -n +3 | sed '/^ template/d' | sed '/^ postgres/d')
     if [ "$exist" == "y" ]
     then
-       read -p "database $newbase already exists, rewrite?" -r over
+       read -p "database $newbase already exists, rewrite? y/n" -r over
     fi
 done
-
-psql -U postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$newbase' AND pid <> pg_backend_pid();"
-dropdb -U postgres $newbase
+if [ "$over" == "y" ]
+then
+    #psql -U postgres -c "ALTER DATABASE $newbase CONNECTION LIMIT 0;"    
+    #psql -U postgres -c "ALTER DATABASE $newbase WITH ALLOW_CONNECTIONS false;"
+    psql -U postgres -c "UPDATE pg_database SET datallowconn = 'false' WHERE datname = '$newbase';"
+    psql -U postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$newbase' AND pid <> pg_backend_pid();"
+    dropdb -U postgres $newbase
+fi
 createdb -U postgres $newbase
-pg_restore -j 4 -d $newbase -U postgres /root/$base$day.bak
+if [ $# -eq 0 ]
+then
+    pg_restore -j 4 -d $newbase -U postgres $apath$day$base.bak
+else
+    if [ ! -f "$1" ]
+    then
+        echo "Check backup file"
+    else
+        pg_restore -j 4 -d $newbase -U postgres $1
+    fi
+fi
